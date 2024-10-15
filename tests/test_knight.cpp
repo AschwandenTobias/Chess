cf333a84f6a07614531f4d8bfbfa244266d71c29
AschwandenTobias <tobias.aschwanden@stud.unibas.ch>
2024-10-15 22:20:34 +0200

    Work on Knight tests

--- tests/test_knight.cpp
+++ tests/test_knight.cpp
@@ -23,6 +23,29 @@ TEST(KnightTest, InitialKinghtPositionsAndMoves) {
     Knight::moveBlackKnight(board, 62, 79);
     Knight::moveBlackKnight(board, 62, 45);
     Knight::moveBlackKnight(board, 57, 42);
-
+    EXPECT_EQ(board.blackKnights, 0x0000240000000000);
 }
 
+TEST(KnightTest, CaptureChecks) {
+    Chessboard board;
+    board.whiteKnights = 0x0000220000000000;
+    board.blackKnights = 0x0000000000220000;
+    Knight::moveWhiteKnight(board, 41, 56);
+    Knight::moveWhiteKnight(board, 56, 41);
+    Knight::moveWhiteKnight(board, 41, 47);
+    Knight::moveWhiteKnight(board, 47, 41);
+    Knight::moveWhiteKnight(board, 41, 51);
+    Knight::moveWhiteKnight(board, 51, 41);
+    Knight::moveWhiteKnight(board, 41, 58);
+    Knight::moveWhiteKnight(board, 58, 41);
+    Knight::moveWhiteKnight(board, 45, 60);
+    Knight::moveWhiteKnight(board, 60, 45);
+    Knight::moveWhiteKnight(board, 45, 51);
+    Knight::moveWhiteKnight(board, 51, 45);
+    Knight::moveWhiteKnight(board, 45, 55);
+    Knight::moveWhiteKnight(board, 55, 45);
+    Knight::moveWhiteKnight(board, 45, 62);
+    Knight::moveWhiteKnight(board, 62, 45);
+    board.whiteKnights = 0x0000220000000000;
+
+}
