24a1f9e02ce7904c59adde810da1b1e79620467b
AschwandenTobias <tobias.aschwanden@stud.unibas.ch>
2024-10-22 21:26:31 +0200

    Add black King movement

--- src/king.cpp
+++ src/king.cpp
@@ -30,3 +30,33 @@ bool King::isWhiteKingMoveLegal(Chessboard &board, int startSquare, int endSquar
     }
     return false;
 }
+
+void King::moveBlackKing(Chessboard &board, int startSquare, int endSquare) {
+    Bitboard from = 1ULL << startSquare;
+    Bitboard to = 1ULL << endSquare;
+    if(isBlackKingMoveLegal(board, startSquare, endSquare)) {
+        board.updateLastMove(startSquare, endSquare);
+        if(board.checkIfWhitePieceIsOnSquare(endSquare)) {
+            board.deletePiece(to);
+        }
+        from = ~from;
+        board.blackKing &= from;
+        board.blackKing |= to;
+    }
+}
+
+bool King::isBlackKingMoveLegal(Chessboard &board, int startSquare, int endSquare) {
+    Bitboard from = 1ULL << startSquare;
+    Bitboard to = 1ULL << endSquare;
+    if(endSquare < 0 || endSquare > 63) return false;
+    if(!board.checkIfBlackKingIsOnSquare(from)) return false;
+    if(board.checkIfBlackPieceIsOnSquare(to)) return false;
+    int distance = std::abs(endSquare - startSquare);
+    int startRow = startSquare / 8;
+    int endRow = endSquare / 8;
+    if (std::abs(endRow - startRow) > 1) return false;
+    if(distance == 1 || distance == 7 || distance == 8 || distance == 9) {
+        return true;
+    }
+    return false;
+}
--- src/king.h
+++ src/king.h
@@ -7,6 +7,8 @@ class King {
 public:
     static void moveWhiteKing(Chessboard &board, int startSquare, int endSquare);
     static bool isWhiteKingMoveLegal(Chessboard &board, int startSquare, int endSquare);
+    static void moveBlackKing(Chessboard &board, int startSquare, int endSquare);
+    static bool isBlackKingMoveLegal(Chessboard &board, int startSquare, int endSquare);
 };
 
 #endif
