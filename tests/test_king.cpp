479b58948153912c0db87707079180dba79f16f1
AschwandenTobias <tobias.aschwanden@stud.unibas.ch>
2024-11-08 17:43:44 +0100

    Add more tests for King movement and checkmate, found mistake

--- tests/test_king.cpp
+++ tests/test_king.cpp
@@ -6,6 +6,7 @@
 #include "pieces/Queen.h"
 #include "pieces/Knight.h"
 #include "pieces/Rook.h"
+#include <utility> // For std::pair
 
 TEST(KingTest, TestWhiteKingMovements) {
     Chessboard board;
@@ -39,7 +40,7 @@ TEST(KingTest, TestNumberOfAttackingPieces_OnePiece) {
     Pawn::moveWhitePawn(game.board, 11, 19);
     Pawn::moveBlackPawn(game.board, 50, 42);
     Queen::moveWhiteQueen(game.board, 4, 32);
-    game.board.printBoard();
+    //game.board.printBoard();
     ASSERT_EQ(King::checkWhiteQueenMovesForCheck(game.board, 59), true); //Check if Queen can check the King
     ASSERT_EQ(King::numberOfAttackingPieces(game.board, 59, false), 1); //Move the whiteQueen to check the black King.
 }
@@ -54,15 +55,49 @@ TEST(KingTest, TestNumberOfAttackingPieces_TwoPieces) {
     Rook::moveBlackRook(game.board, 56, 40);
     Rook::moveBlackRook(game.board, 40, 43);
     Knight::moveBlackKnight(game.board, 19, 9);
-    game.board.printBoard();
+    //game.board.printBoard();
     ASSERT_EQ(King::numberOfAttackingPieces(game.board, 3, true), 2);
 }
 
 TEST(KingTest, TestGetAttackingSquares) {
-
+    Game game;
+    Pawn::moveWhitePawn(game.board, 11, 19);
+    Pawn::moveBlackPawn(game.board, 50, 42);
+    Queen::moveWhiteQueen(game.board, 4, 32);
+    game.board.printBoard();
+    std::vector<int> shouldSquares = {32, 41, 50};
+    std::vector<int> attackingSquares = Queen::getAttackingSquares(game.board, 32, 59);
+    //std::cout << "Number of attacking Squares: " << attackingSquares.size() << "\n";
+    EXPECT_EQ(shouldSquares.size(), attackingSquares.size());
+    EXPECT_EQ(shouldSquares, attackingSquares);
 }
 
-TEST(KingTest, TestCanPieceInterfere) {
-
+TEST(KingTest, TestCanPieceInterfere_WhiteQueenOnBorder) {
+    Game game;
+    Pawn::moveWhitePawn(game.board, 11, 19);
+    Pawn::moveBlackPawn(game.board, 50, 42);
+    Queen::moveWhiteQueen(game.board, 4, 32);
+    game.board.printBoard();
+    EXPECT_EQ(King::canPieceInterfereCheck(game.board, true), false); //no piece should be able to interfere
 }
 
+TEST(KingTest, GeneratedPossibleKingMoves_OnStartSquare) {
+    Game game;
+    std::vector<std::pair<int, int>> shouldSquares = {};
+    EXPECT_EQ(King::generateAllPossibleKingMoves(game.board, true), shouldSquares);
+}
+
+
+TEST(KingTest, GeneratedPossibleKingMoves_OneSquareForward) {
+    Game game;
+    Pawn::moveWhitePawn(game.board, 11, 19);
+    std::vector<std::pair<int, int>> shouldSquares = {{3, 11}};
+    EXPECT_EQ(King::generateAllPossibleKingMoves(game.board, true), shouldSquares);
+}
+
+TEST(KingTest, GeneratedPossibleKingMoves_OneBoardBorder) {
+    Game game;
+    game.board.whiteKing = 0x0000000001000000;
+    std::vector<std::pair<int, int>> shouldSquares = {{24, 31}, {24, 17}, {24, 16}, {24, 25}, {24, 33}};
+    EXPECT_EQ(King::generateAllPossibleKingMoves(game.board, true), shouldSquares);
+}
