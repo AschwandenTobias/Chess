2a4b6704f84eed4351255da7847f58479bfc89c4
AschwandenTobias <tobias.aschwanden@stud.unibas.ch>
2025-04-14 20:34:52 +0200

    Setup files

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to this little chess game! So far nothing is implemented and nothing runs :(. But im sure i will eventually implement everything and not just sit on this project forever :)\n";
+}
