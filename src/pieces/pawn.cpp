297c3728247a4d16c45e7309320de4171d97a145
Tobias Aschwanden <tobias.aschwanden@stud.unibas.ch>
2025-10-09 21:54:02 +0200

    Stop pawns from warping around the board

--- src/pieces/pawn.cpp
+++ src/pieces/pawn.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "pawn.h"
 
-//TODO: Finish this
+//TODO: Implement en passant
 bool Pawn::isMoveValid(const Move& move, const Position& position, bool white) {
     //Check for en passant
     //Check for promotion
@@ -9,6 +9,7 @@ bool Pawn::isMoveValid(const Move& move, const Position& position, bool white) {
     int from = move.from;
     int to = move.to;
     int distance = to - from;
+    int fromFile = from % 8;
     //std::cout << "pawn.cpp: Pawn movement distance: " << distance << "\n";
     if(white) {
         //std::cout << "pawn.cpp: White pawn wants to move\n";
@@ -18,11 +19,17 @@ bool Pawn::isMoveValid(const Move& move, const Position& position, bool white) {
         if(distance == 16 && from <= 15 && from >= 8 && !position.isOccupied(to) && !position.isOccupied(from + 8)) return true;
         //std::cout << "pawn.cpp: Checking if capture is possible\n";  
         //std::cout << "pawn.cpp: Check if to is occupied by black piece: " << position.isOccupiedByBlackPiece(to) << "\n";  
-        if((distance == 7 || distance == 9) && position.isOccupiedByBlackPiece(to)) return true;
+        if (distance == 7 && fromFile != 0 && position.isOccupiedByBlackPiece(to))
+            return true;
+        if (distance == 9 && fromFile != 7 && position.isOccupiedByBlackPiece(to))
+            return true;
     } else {
         if(distance == -8 && !position.isOccupied(to)) return true;
         if(distance == -16 && from <= 56 && from >= 49 && !position.isOccupied(to) && !position.isOccupied(from - 8)) return true;
-        if((distance == -7 || distance == -9) && position.isOccupiedByWhitePiece(to)) return true;
+        if (distance == -7 && fromFile != 7 && position.isOccupiedByWhitePiece(to))
+            return true;
+        if (distance == -9 && fromFile != 0 && position.isOccupiedByWhitePiece(to))
+            return true;    
     }
     //std::cout << "pawn.cpp: Move wasn't legal, returning false \n";
     return false;
